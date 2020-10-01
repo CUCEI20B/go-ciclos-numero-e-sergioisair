e141bc5317f6a04c186b8e5c21c6d0baee6208ea
Sergio Isair <sergioisair@gmail.com>
2020-10-01 15:34:07 -0500

    Subida de archivo

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var n int
+	var e float64 = 0
+	var x float64 = 1
+	fmt.Scan(&n)
+	for i := 0; i <= n; i++ {
+		x = 1
+		for j := i; j >= 1; j-- {
+			x = x * float64(j)
+		}
+		e = e + (1 / x)
+	}
+	fmt.Print(e)
 }
